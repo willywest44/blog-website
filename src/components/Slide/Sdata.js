f9de0766d8b0d7c195f0c37425e1b83d54dcda1a
D_defender <olamidewilliams2001@gmail.com>
2024-04-13 11:43:17 +0100

    Update Sdata.js

--- src/components/Slide/Sdata.js
+++ src/components/Slide/Sdata.js
@@ -1,21 +1,21 @@
 const Sdata = [
     {
       id: 1,
-      cover: "/Images/slide-1.jpg",
+      cover: "/Images/blog1.jpg",
       category: "lifestyle",
       title: "What are the best countries to visit?",
       desc: "In this post, we are going to highlight the many benefits of blogging for business and how you can get started with",
     },
     {
       id: 2,
-      cover: "/Images/slide-2.jpg",
+      cover: "/Images/blog3.jpg",
       category: "lifestyle",
       title: "What are the best countries to visit?",
       desc: "In this post, we are going to highlight the many benefits of blogging for business and how you can get started with",
     },
     {
       id: 3,
-      cover: "/Images/slide-3.jpg",
+      cover: "/Images/cart-4.jpg",
       category: "lifestyle",
       title: "What are the best countries to visit?",
       desc: "In this post, we are going to highlight the many benefits of blogging for business and how you can get started with",
